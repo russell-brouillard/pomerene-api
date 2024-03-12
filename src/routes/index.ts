f4247180d08fb82b98d91ac1b1e0f2ebdd3491aa
russell-brouillard <rbrouillard@crunchtime.com>
2024-03-12 09:53:03 -0600

    test

--- src/routes/index.ts
+++ src/routes/index.ts
@@ -14,6 +14,7 @@ import {
   getUserJWTController,
 } from "../controllers/userController";
 import { createDeviceController } from "../controllers/deviceController";
+import cors from "cors";
 
 const router = express.Router();
 
@@ -25,7 +26,15 @@ router.get("/spl-tokens/:publicKey", getSPLTokensController);
 //user endpoints
 router.get("/user/solana-keypair", authMiddleware, getSolanaKeypair);
 router.post("/user/create", createUserWithSolanaKeypair);
-router.post("/user/signIn", getUserJWTController);
+
+router.post("/user/signIn", cors(), (req, res) => {
+  // Set Access-Control-Allow-Origin header to allow requests from specific origins
+  
+  res.setHeader("Access-Control-Allow-Origin", "https://www.pomerene.net");
+  console.log("req", req.headers);
+  // Call your controller function
+  getUserJWTController(req, res);
+});
 router.get("/user/email/:email", getUserByEmailController);
 router.get("/user/:uid", getUserByUIDController);
 router.get("/users", getAllUsersController);
