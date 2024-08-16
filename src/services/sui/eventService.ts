748a188d7990829d14d5ded94bd6e4ccdc7586fb
Russell Brouillard <rssll.brllrd@gmail.com>
2024-08-16 02:40:33 -0600

    🤠

--- src/services/sui/eventService.ts
+++ src/services/sui/eventService.ts
@@ -3,7 +3,7 @@ import { Transaction } from "@mysten/sui/transactions";
 import { getSuiKeypairFromSecret } from "./suiService";
 import { getFullnodeUrl, SuiClient } from "@mysten/sui/client";
 import { Ed25519PublicKey } from "@mysten/sui/keypairs/ed25519";
-import { fromHEX } from "@mysten/sui/utils";
+import { fromHEX, toHEX } from "@mysten/sui/utils";
 
 export async function createSuiScannerTransaction(
   scannerSecret: string,
@@ -13,7 +13,7 @@ export async function createSuiScannerTransaction(
   const rpcUrl = getFullnodeUrl("devnet");
   const client = new SuiClient({ url: rpcUrl });
 
-  const { gpsData, combinedSignature } = await signGPSData(
+  const combinedSignature = await signGPSData(
     scannerSecret,
     itemSecret,
     message
@@ -22,16 +22,10 @@ export async function createSuiScannerTransaction(
   const nftId = await createScanNFT(
     scannerSecret,
     itemSecret,
-    gpsData,
+    message,
     combinedSignature,
     client
   );
-
-  // Retrieve and validate NFT later
-  // const response = await client.getOwnedObjects({
-  //   owner: scannerKeypair.getPublicKey().toSuiAddress(),
-  //   options: { showContent: true },
-  // });
 
   const valid = validateGPSDataFromNFT(client, nftId);
 
@@ -41,19 +35,30 @@ export async function createSuiScannerTransaction(
 export async function signGPSData(
   scannerSecret: string,
   itemSecret: string,
-  gpsData: string
+  message: string
 ) {
   const scannerKeypair = getSuiKeypairFromSecret(scannerSecret);
   const itemKeypair = getSuiKeypairFromSecret(itemSecret);
+
+  const scannerPublicKey = scannerKeypair.getPublicKey();
+  const itemPublicKey = itemKeypair.getPublicKey();
+
+  console.log("Order during signing:");
+  console.log(
+    "Scanner Public Key:",
+    scannerKeypair.getPublicKey().toSuiAddress()
+  );
+  console.log("Item Public Key:", itemKeypair.getPublicKey().toSuiAddress());
+
   const multiSigPublicKey = MultiSigPublicKey.fromPublicKeys({
-    threshold: 1, // You can increase the threshold if you want both signatures to be required
+    threshold: 1,
     publicKeys: [
-      { publicKey: scannerKeypair.getPublicKey(), weight: 1 },
-      { publicKey: itemKeypair.getPublicKey(), weight: 1 },
+      { publicKey: scannerPublicKey, weight: 1 },
+      { publicKey: itemPublicKey, weight: 1 },
     ],
   });
 
-  const encodedMessage = new TextEncoder().encode(gpsData);
+  const encodedMessage = new TextEncoder().encode(message);
 
   const signatureScanner = (
     await scannerKeypair.signPersonalMessage(encodedMessage)
@@ -68,7 +73,18 @@ export async function signGPSData(
     signatureItem,
   ]);
 
-  return { gpsData, combinedSignature };
+  console.log("storedSignature 1", combinedSignature);
+  console.log("encodedMessage 1", encodedMessage);
+  console.log("multiSigPublicKey 1", multiSigPublicKey);
+
+  const isValid = await multiSigPublicKey.verifyPersonalMessage(
+    encodedMessage,
+    combinedSignature
+  );
+
+  console.log("VALID? ", isValid);
+
+  return combinedSignature;
 }
 
 export async function createScanNFT(
@@ -81,18 +97,26 @@ export async function createScanNFT(
   const scannerKeypair = getSuiKeypairFromSecret(scannerSecret);
   const itemKeypair = getSuiKeypairFromSecret(itemSecret);
 
+  console.log("message 1 ", message);
+  
+
+
+  console.log(scannerKeypair.getPublicKey().toRawBytes())
+  console.log(toHEX(scannerKeypair.getPublicKey().toRawBytes()))
+  console.log("DONE")
+
   // Mint an NFT and embed the message and signatures
   const tx = new Transaction();
   tx.moveCall({
     package:
-      "0x5d1558120005ba6d93824c954d9ff2cd0c764458014ffe64fab6c248976a47fb", // Replace with your package address
+      "0xd7511cf3a30f5ad6003a6a8a58f21513351cec5b213252b78ede08fe8f673962", // Replace with your package address
     module: "pomerene",
     function: "mint_to_sender", // Assume this function exists to mint your NFT
     arguments: [
-      tx.pure.string("PomeNFT"),
-      tx.pure.string("https://example.com/nft-image.png"),
-      tx.pure.address(scannerKeypair.getPublicKey().toSuiAddress()),
-      tx.pure.address(itemKeypair.getPublicKey().toSuiAddress()),
+      tx.pure.string("PomeScan"),
+      tx.pure.string("https://www.pomerene.net/white-small.png"),
+      tx.pure.address(toHEX(scannerKeypair.getPublicKey().toRawBytes())),
+      tx.pure.address(toHEX(itemKeypair.getPublicKey().toRawBytes())),
       tx.pure.string(message), // Embed the original message
       tx.pure.string(combinedSignature), // Embed the combined signature
     ],
@@ -103,15 +127,11 @@ export async function createScanNFT(
     signer: scannerKeypair,
   });
 
-  console.log("Initial Transaction Result: ", initialResult);
-
   // Wait for the transaction to be confirmed and processed
   const finalResult = await client.waitForTransaction({
     digest: initialResult.digest,
     options: { showEffects: true, showEvents: true },
   });
-
-  console.log("Final Transaction Result: ", finalResult);
 
   // Extract the new NFT ID from the transaction response
   const nftId = finalResult.effects?.created?.[0]?.reference?.objectId;
@@ -134,27 +154,36 @@ async function validateGPSDataFromNFT(client: SuiClient, nftId: string) {
 
   const content = nftDetails.data?.content?.fields;
   if (content) {
-    const storedGPSData = content.message; // Assuming GPS data is stored here
+    const message = content.message; // Assuming GPS data is stored here
     const storedSignature = content.combinedSignature; // Assuming signature is stored here
+
+    console.log("message 2", message);
 
     const scannerPK = content.scanner;
     const itemPK = content.item;
 
-    console.log("scannerPK ", itemPK);
+    console.log("scannerPK ", scannerPK);
     console.log("itemPK ", itemPK);
 
     const scannerPublicKey = new Ed25519PublicKey(fromHEX(scannerPK));
     const itemPublicKey = new Ed25519PublicKey(fromHEX(itemPK));
 
+    console.log("Scanner Public Key:", scannerPublicKey.toSuiAddress());
+    console.log("Item Public Key:", itemPublicKey.toSuiAddress());
+
     const multiSigPublicKey = MultiSigPublicKey.fromPublicKeys({
-      threshold: 1, // Adjust the threshold if both signatures are required
+      threshold: 1,
       publicKeys: [
         { publicKey: scannerPublicKey, weight: 1 },
         { publicKey: itemPublicKey, weight: 1 },
       ],
     });
 
-    const encodedMessage = new TextEncoder().encode(storedGPSData);
+    const encodedMessage = new TextEncoder().encode(message);
+
+    console.log("Order during validation:");
+
+    console.log("multiSigPublicKey 2", multiSigPublicKey);
 
     // Validate the signature using the original MultiSigPublicKey
     const isValid = await multiSigPublicKey.verifyPersonalMessage(
